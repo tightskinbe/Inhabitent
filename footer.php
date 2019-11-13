58829655a91d885af2dca0109661c658b9b0d1c4
tightskinbe <wowmeupeight@gmail.com>
2019-11-13 09:51:01 -0800

    init

--- footer.php
+++ footer.php
@@ -13,6 +13,11 @@
 				<div class="site-info">
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
+				<div>
+					<h3><i class="fas fa-envelope"></i> Contact Info</h3>
+					<p><i class="fas fa-phone-alt"></i> 778-456-7891</p>
+					<p><i class="fab fa-facebook-square"></i><i class="fab fa-twitter-square"></i><i class="fab fa-google-plus-square"></i></p>
+				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
 
--- header.php
+++ header.php
@@ -12,6 +12,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+    integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
 
 	<?php wp_head(); ?>
 	</head>
